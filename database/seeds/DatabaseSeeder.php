ad4d3381ad88ff02aa5a7257f176367b0824fab0
Freakadoom <openes2957@yandex.ru>
2020-05-19 18:58:58 +0300

    Сиды на города, регионы, цвет, виды и породы.

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(BreedsSeeder::class);
+        $this->call(ColorsSeeder::class);
+        $this->call(SpeciesSeeder::class);
+        $this->call(RegionsSeeder::class);
+        $this->call(CitiesSeeder::class);
+    }
+}
