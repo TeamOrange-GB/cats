faa500c3045cf416bc941644cc235cadfdcfd5c1
Grigoriy mikirtumov <mikigrigorij@yandex.ru>
2020-05-21 19:26:22 +0300

    slider general page

--- frontend/src/components/vMain.vue
+++ frontend/src/components/vMain.vue
@@ -1,15 +1,47 @@
 <template>
     <section class="v-main">
-        <h1>Главная секция!</h1>
+        
+            <Slider/>
+            <!-- <div class="slider__element">
+                <img src="https://via.placeholder.com/600x400" alt="slide">
+                <button class="slider__btn">Подробнее</button>
+            </div> -->
+            <!-- <div class="slider__element">
+                <img src="https://via.placeholder.com/600x400" alt="slide">
+            </div>
+            <div class="slider__element">
+                <img src="https://via.placeholder.com/600x400" alt="slide">
+            </div>
+            <div class="slider__element">
+                <img src="https://via.placeholder.com/600x400" alt="slide">
+            </div>
+            <div class="slider__element">
+                <img src="https://via.placeholder.com/600x400" alt="slide">
+            </div>
+            <div class="slider__element">
+                <img src="https://via.placeholder.com/600x400" alt="slide">
+            </div>
+            <div class="slider__element">
+                <img src="https://via.placeholder.com/600x400" alt="slide">
+            </div>
+            <div class="slider__element">
+                <img src="https://via.placeholder.com/600x400" alt="slide">
+            </div> -->
+        
     </section>
 </template>
 
 <script>
+    import Slider from './vSlider.vue'
     export default {
+        components:{
+            Slider
+        },
+        
         name: "vMain"
     }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+    
 </style>
---
+++ frontend/src/components/vSlider-item.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="slider__element">
+        <p class="slider__element__content">{{slideData.content}}</p>
+        <button class="slider__element__btn">Подробнее</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        slideData:{
+            type: Object
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+    .slider__element{
+            transition-duration: 3s;
+            display: flex;
+            flex-shrink: 0;
+            flex-wrap: wrap;
+            justify-content: center;
+            width: 800px ; 
+            height: 400px;
+            background-color: $color-smoke;
+           
+            &__btn{
+                @include buttonSetting(150px, 50px, $color-cyan, $color-light-grey);
+                margin-bottom: 30px;
+                align-self: flex-end;
+            }
+            &__content{
+                width: 100%;
+                align-self: center;
+                text-align: center;
+            }
+        }
+</style>
