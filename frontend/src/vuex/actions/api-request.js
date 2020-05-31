15ba2e051febd700cebdc18bf17ef6efc79c95c6
Evgeny Borodin <54502039+bor1eu@users.noreply.github.com>
2020-05-31 22:58:18 +0300

    Update api-request.js

--- frontend/src/vuex/actions/api-request.js
+++ frontend/src/vuex/actions/api-request.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export default {
     GET_CATALOG_FROM_API({commit}) {
-        // return axios ('http://cats.xyz/api/catalog', {
-        return axios ('http://localhost:3000/catalog', {
+        return axios ('http://cats.xyz/api/catalog', {
+        // return axios ('http://localhost:3000/catalog', {
             method: "GET"
         })
             .then((catalog) => {
