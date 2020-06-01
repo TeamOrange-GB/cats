761d55f2fd343fba22b65cd93ce9ba6c016a1604
eaglecrazy <eaglecrazy@mail.ru>
2020-06-01 22:44:42 +0300

    Фронтенд5

--- frontend/src/vuex/actions/api-request.js
+++ frontend/src/vuex/actions/api-request.js
@@ -30,20 +30,35 @@ export default {
             })
     },
     SENDING_REGISTRATION_DATA_IN_API(name, email, password, conf_pass) {
-        return axios ('/api/register', {
-            method: "POST",
-            name: name,
-            email: email,
-            password: password,
-            password_confirmation: conf_pass
-        })
-            .then((response) => {
-                console.log(response)
-            })
-            .catch((error) => {
-                console.log(error);
-                return error;
-            })
+        // let response = axios.post ('/register', {
+        //     // method: "POST",
+        //     name: 'name',
+        //     email: 'email',
+        //     password: 'password',
+        //     password_confirmation: 'conf_pass'
+        // })
+        //     .then((response) => {
+        //         console.log(response);
+        //         return response;
+        //     })
+        //     .catch((error) => {
+        //         console.log(error);
+        //         return error;
+        //     })
+
+        let response = axios.post('http://cats.xyz/register', {
+            email: 'email_var',
+            name: 'name_var',
+            password: 'password_var',
+            password_confirmation: 'password_confirmation_var'
+        }).then()
+        {
+            let message = response.data['message'];
+            let status = response.data['status'];
+            let user = response.data['user'];
+            return response;
+        }
+
     },
     FIRST_USER_LOGIN(){
         return axios('http://cats.xyz/api/user', {
