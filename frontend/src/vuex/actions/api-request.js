e590861404173916952270a2b089fe827f9df439
eaglecrazy <eaglecrazy@mail.ru>
2020-06-01 23:38:08 +0300

    Фронтенд7

--- frontend/src/vuex/actions/api-request.js
+++ frontend/src/vuex/actions/api-request.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export default {
     GET_CATALOG_FROM_API({commit}) {
-        return axios ('/api/catalog', {
-        // return axios ('http://localhost:3000/catalog', {
+        return axios('/api/catalog', {
+            // return axios ('http://localhost:3000/catalog', {
             method: "GET"
         })
             .then((catalog) => {
@@ -16,7 +16,7 @@ export default {
             })
     },
     SENDING_AUTH_DATA_IN_API({commit}, result) {
-        return axios ('/api/register', {
+        return axios('/api/register', {
             method: "POST",
             data: result
         })
@@ -30,41 +30,50 @@ export default {
             })
     },
     SENDING_REGISTRATION_DATA_IN_API(name, email, password, conf_pass) {
-        // let response = axios.post ('/register', {
-        //     // method: "POST",
-        //     name: 'name',
-        //     email: 'email',
-        //     password: 'password',
-        //     password_confirmation: 'conf_pass'
-        // })
-        //     .then((response) => {
-        //         console.log(response);
-        //         return response;
-        //     })
-        //     .catch((error) => {
-        //         console.log(error);
-        //         return error;
-        //     })
+        console.log(name);
+        console.log(email);
+        console.log(password);
+        console.log(conf_pass);
 
-        let response = axios.post('http://cats.xyz/register', {
-            email: 'email_var',
-            name: 'name_var',
-            password: 'password_var',
-            password_confirmation: 'password_confirmation_var'
-        }).then()
-        {
-            let message = response.data['message'];
-            let status = response.data['status'];
-            let user = response.data['user'];
+        let response = axios({
+            method: 'post',
+            url: '/register',
+            data: {
+                name: name,
+                email: email,
+                password: password,
+                password_confirmation: conf_pass
+            }
+        })
+        .then((response) => {
+            console.log(response);
             return response;
-        }
+        })
+        .catch((error) => {
+            console.log(error);
+            return error;
+        })
+
+        // let response = axios('http://cats.xyz/register', {
+        //     method: "POST",
+        //     email: 'email_var',
+        //     name: 'name_var',
+        //     password: 'password_var',
+        //     password_confirmation: 'password_confirmation_var'
+        // }).then()
+        // {
+        //     let message = response.data['message'];
+        //     let status = response.data['status'];
+        //     let user = response.data['user'];
+        //     return response;
+        // }
 
     },
-    FIRST_USER_LOGIN(){
+    FIRST_USER_LOGIN() {
         return axios('http://cats.xyz/api/user', {
             method: "GET"
         })
-            .then((response)=> {
+            .then((response) => {
                 console.log(response);
                 return response;
             })
