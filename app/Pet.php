702aa8d60c0b7258688d65cb5da860a53164669d
eaglecrazy <eaglecrazy@mail.ru>
2020-06-06 18:26:45 +0300

    Установил Laravel ide helper

--- app/Pet.php
+++ app/Pet.php
@@ -4,6 +4,60 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Pet
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @property string $birthday_at Дата рождения
+ * @property int $gender Пол животного true - М, false - Ж
+ * @property int $mating Возможна ли вязка
+ * @property string $name_real Кличка в жизни
+ * @property string|null $name_doc Кличка по документам
+ * @property string|null $titles Титулы
+ * @property string|null $description Описание
+ * @property string|null $pedigree Ссылка на фото родословной
+ * @property string|null $certificate Ссылка на фото сертификата
+ * @property string|null $veterinary_certificate Ссылка на фото ветеринарного сертификата
+ * @property string|null $metrics Ссылка на фото метрики
+ * @property int $likes_count Количество лайков
+ * @property string $class Класс животного, pet обычное животное
+ * @property string|null $awards_site Награды на сайте
+ * @property string|null $awards Другие награды
+ * @property string $status Статус животного
+ * @property int $species_id Id вида
+ * @property int $breed_id Id породы
+ * @property int $color_id Id окраса
+ * @property int $user_id Id хозяина
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereAwards($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereAwardsSite($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereBirthdayAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereBreedId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereCertificate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereClass($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereColorId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereGender($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereLikesCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereMating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereMetrics($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereNameDoc($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereNameReal($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet wherePedigree($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereSpeciesId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereTitles($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Pet whereVeterinaryCertificate($value)
+ * @mixin \Eloquent
+ */
 class Pet extends Model
 {
     protected $table = 'pets';
--- app/User.php
+++ app/User.php
@@ -6,6 +6,52 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @property string $name Имя пользователя
+ * @property string|null $email E-mail пользователя
+ * @property string $password Пароль пользователя
+ * @property string $role Полномочия пользователя
+ * @property string|null $link_vkontakte ссылка на В контакте
+ * @property string|null $link_odnoklassniki ссылка на Одноклассники
+ * @property string|null $link_google Ссылка на google
+ * @property string|null $social_id Id в социальной сети, через которую была регистрация
+ * @property string $type_auth Тип авторизации
+ * @property string $status Статус пользователя
+ * @property string|null $description Описание пользователя
+ * @property string|null $phone Телефон пользователя
+ * @property string|null $avatar Ссылка на аватар
+ * @property int|null $city_id Id населённого пункта
+ * @property string|null $remember_token
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCityId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereLinkGoogle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereLinkOdnoklassniki($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereLinkVkontakte($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRole($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereSocialId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereTypeAuth($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
